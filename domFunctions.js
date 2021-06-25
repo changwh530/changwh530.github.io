70e6c65bac27cfa119e8f88109106514ec6ef763
changwh530 <86424559+changwh530@users.noreply.github.com>
2021-06-25 13:15:19 +0800

    Add files via upload

---
+++ domFunctions.js
@@ -0,0 +1,19 @@
+function toEarth(){
+    window.location.href='map4-加载整个地球.html';
+}
+
+function toDataPage(){
+    window.location.href='map6-难民地理信息.html';
+}
+
+function toPay(){
+    window.location.href='http://106.14.149.85/Bonus.html';
+}
+
+function toInfo(){
+    window.location.href='information.html';
+}
+
+function downInfo(){
+    window.location.href='selfInfo.pdf';
+}
